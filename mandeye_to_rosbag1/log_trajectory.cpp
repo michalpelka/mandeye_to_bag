25e185748ac799b39ab034baddca07ef0d703e6f
Michal Pelka <michalpelka@gmail.com>
2024-08-10 10:36:01 +0200

    log trajectory
    Signed-off-by: Michal Pelka <michalpelka@gmail.com>

---
+++ mandeye_to_rosbag1/log_trajectory.cpp
@@ -0,0 +1,28 @@
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <fstream>
+
+void odometryCallback(const nav_msgs::Odometry& msg)
+{
+    // Log the trajectory points to the file
+
+        const auto& position = msg.pose.pose.position;
+        const auto& orientation = msg.pose.pose.orientation;
+
+        std::cout << position.x << " " << position.y << " " << position.z << " ";
+        std::cout << orientation.x << " " << orientation.y << " " << orientation.z << " " << orientation.w << std::endl;
+
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "trajectory_logger_node");
+    ros::NodeHandle nh;
+    ros::Subscriber sub = nh.subscribe("/Odometry", 10, odometryCallback);
+
+    ros::spin();
+
+
+    return 0;
+}
+
