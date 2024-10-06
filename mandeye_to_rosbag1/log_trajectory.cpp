e4cfeeeb5a7d059dea94f88dabadc67d9f07bb3d
Michał Pełka <michalpelka@gmail.com>
2024-10-07 00:54:12 +0200

    Update log_trajectory.cpp

--- mandeye_to_rosbag1/log_trajectory.cpp
+++ mandeye_to_rosbag1/log_trajectory.cpp
@@ -2,16 +2,17 @@
 #include <nav_msgs/Odometry.h>
 #include <fstream>
 
+std::ofstream file;
 void odometryCallback(const nav_msgs::Odometry& msg)
 {
     // Log the trajectory points to the file
 
         const auto& position = msg.pose.pose.position;
         const auto& orientation = msg.pose.pose.orientation;
-        std::cout << msg.header.stamp.toSec() <<std::endl;
-        std::cout << position.x << " " << position.y << " " << position.z << " ";
-        std::cout << orientation.x << " " << orientation.y << " " << orientation.z << " " << orientation.w << " ";
-        std::cout <<std::endl;
+        file << msg.header.stamp.toSec() <<std::endl;
+        file << position.x << " " << position.y << " " << position.z << " ";
+        file << orientation.x << " " << orientation.y << " " << orientation.z << " " << orientation.w << " ";
+        file <<std::endl;
 
 }
 
@@ -19,7 +20,19 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "trajectory_logger_node");
     ros::NodeHandle nh;
-    ros::Subscriber sub = nh.subscribe("/Odometry", 10, odometryCallback);
+    std::string file_path = "trajectory.txt";
+    nh.getParam("file_path", file_path);
+    std::cout << "File path: " << file_path << std::endl;
+    file.open(file_path);
+    
+    std::cout << "Logging trajectory to file: " << file_path << std::endl;
+
+    std::string topic_name = "Odometry";
+    nh.getParam("topic_name", topic_name);
+    std::cout << "Topic name: " << topic_name << std::endl;
+
+
+    ros::Subscriber sub = nh.subscribe(topic_name, 10, odometryCallback);
 
     ros::spin();
 
