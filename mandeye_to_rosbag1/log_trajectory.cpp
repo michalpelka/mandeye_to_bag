7960b30a084c4c9fcb9d5ee289ae0dca572bf407
Michał Pełka <michalpelka@gmail.com>
2024-10-11 02:07:48 +0200

    Removed space (#14)

--- mandeye_to_rosbag1/log_trajectory.cpp
+++ mandeye_to_rosbag1/log_trajectory.cpp
@@ -9,9 +9,9 @@ void odometryCallback(const nav_msgs::Odometry& msg)
 
         const auto& position = msg.pose.pose.position;
         const auto& orientation = msg.pose.pose.orientation;
-        file << (uint64_t) msg.header.stamp.toNSec() <<", ";
-        file << position.x << ", " << position.y << ", " << position.z << ", ";
-        file << orientation.x << ", " << orientation.y << ", " << orientation.z << ", " << orientation.w << std::endl;
+        file << (uint64_t) msg.header.stamp.toNSec() <<",";
+        file << position.x << "," << position.y << "," << position.z << ",";
+        file << orientation.x << "," << orientation.y << "," << orientation.z << "," << orientation.w << std::endl;
 
 }
 
